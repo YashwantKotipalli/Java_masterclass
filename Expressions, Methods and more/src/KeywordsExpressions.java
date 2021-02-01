5956c2d6678ced9adfada6d08da8d62294474ca3
YashwantKotipalli <kotipalli.ve@northeastern.edu>
2021-01-31 21:15:05 -0500

    adding changes to the new file

---
+++ Expressions, Methods and more/src/KeywordsExpressions.java
@@ -0,0 +1,23 @@
+public class KeywordsExpressions {
+
+    public static void main(String[] args ){
+
+        // 1 mile = 1.609344 kilometers
+
+        double miles = 100d;
+        double kilometers = 1.609344d*miles;
+        // datatype expression
+        // expression in java consists of variables, values and operators
+        // adding a datatype to an expression and a semicolon at the end of an expression makes it a statement in java
+        System.out.println(kilometers);
+
+        int highscore = 50;
+        if(highscore >=50){
+            System.out.println("This is an expression as well");
+        }
+
+
+
+
+    }
+}
---
+++ Expressions, Methods and more/src/statementsWhitespacesIndentations.java
@@ -0,0 +1,16 @@
+public class statementsWhitespacesIndentations {
+
+    public static void main(String[] args){
+
+        // statements in java
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is an expression");
+
+        System.out.println("This is another statement in java");
+
+
+    }
+}
