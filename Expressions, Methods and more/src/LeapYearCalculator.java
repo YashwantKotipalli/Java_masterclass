d3447303c3364003f5bc9ec46a3b9bf427d6f886
YashwantKotipalli <kotipalli.ve@northeastern.edu>
2021-02-03 15:45:51 -0500

    adding files

---
+++ Expressions, Methods and more/src/LeapYearCalculator.java
@@ -0,0 +1,25 @@
+public class LeapYearCalculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLeapYear(int year) {
+
+        if((year >=1) && (year<=9999)){
+
+            if(year%4==0){
+                if(year%100==0){
+                    if(year%400==0){
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+        return false;
+
+    }
+}
