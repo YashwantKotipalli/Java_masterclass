da06891d6258ca4ac6d21312a0de0ca253529239
YashwantKotipalli <kotipalli.ve@northeastern.edu>
2021-02-03 15:34:42 -0500

    adding files

---
+++ Expressions, Methods and more/src/BarkingDog.java
@@ -0,0 +1,17 @@
+public class BarkingDog {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else if ((hourOfDay < 8 || hourOfDay > 22) && barking) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
